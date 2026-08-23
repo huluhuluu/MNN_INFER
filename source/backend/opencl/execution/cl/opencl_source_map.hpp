ccc61ee0fa320b37d328b771ccabd28d38d7bd7e
huluhuluu <hustzjh@hust.edu.cn>
2026-08-23 17:27:44 +0800

    opencl: regenerate cl source map, ignore FA result dirs

--- source/backend/opencl/execution/cl/opencl_source_map.hpp
+++ source/backend/opencl/execution/cl/opencl_source_map.hpp
@@ -344,7 +344,7 @@ const std::map<std::string, std::string> OpenCLProgramMd5Map =
   { "scale", "95773334e603db663c594945a064b9cc" },
   { "grid_sample_buf", "8b4bd3d6f9154b6dc6fafac3fce1c11e" },
   { "nearest", "e8b2081c5e50ae6d370989f816cda543" },
-  { "attention_buf", "27c43a684ed375a99e3616175695da1b" },
+  { "attention_buf", "34f2ea2446743d02534ff8f1e42406de" },
   { "reduction", "222cc3d09d2d5f2e0db4679a17aa8628" },
   { "roi_pooling", "ba4a81b7ec7058d14afb377c18674a76" },
   { "buffer_convert_buf", "e633544642a1a9a61755c913cfe77017" },
